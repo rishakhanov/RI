20e2e98fe68c3a8fda3d5a2ae6a7450253bb1718
rishakhanov <rishakhanov@rambler.ru>
2018-08-18 21:24:08 +0600

    5.2. Создать программу вычисляющую факториал. [#193]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -0,0 +1,24 @@
+package ru.job4j.loop;
+
+/**
+ * @author Rajes Ishakhanov (rishakhanov@rambler.ru)
+ * @version $ID$
+ * @since 0.1
+ */
+ 
+public class Factorial {
+	/**
+	 * Подсчет суммы чётных чисел в диапазоне.
+	 * @param start,finish Числа
+	 * @return Сумма чётных чисел
+	 */
+	public int calc(int n){
+		int sum=1;
+		if (n==0) sum = 1;
+		else {
+			for (int i=1; i<=n;i++) sum=sum*i; 
+		}		
+		return sum;	
+	}
+		
+}	
---
+++ chapter_001/src/test/java/ru/job4j/loop/FactorialTest.java
@@ -0,0 +1,58 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Rajes Ishakhanov (rishakhanov@rambler.ru)
+ * @version $ID$
+ * @since 0.1
+ */
+ 
+public class FactorialTest {
+	@Test
+	public void whenCalculateFactorialForFiveThenOneHundreedTwenty(){
+	Factorial factorial = new Factorial();
+	int result = factorial.calc(5);
+	assertThat(result, is(120));
+	}
+
+	@Test
+	public void whenCalculateFactorialForZeroThenOne(){
+	Factorial factorial = new Factorial();
+	int result = factorial.calc(0);
+	assertThat(result, is(1));
+	}
+	
+}	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
