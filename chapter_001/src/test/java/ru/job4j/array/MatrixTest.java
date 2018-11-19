01c2797c023a57c58f504422383e72dad6f22b58
rishakhanov <rishakhanov@rambler.ru>
2018-11-19 13:23:56 +0600

    6.6. Двухмерный массив. Таблица умножения. [#33491]_updated

--- chapter_001/src/test/java/ru/job4j/array/MatrixTest.java
+++ chapter_001/src/test/java/ru/job4j/array/MatrixTest.java
@@ -15,4 +15,43 @@ public class MatrixTest {
         };
         assertThat(table, is(expect));
     }
+
+    @Test
+    public void when3on3() {
+        Matrix matrix = new Matrix();
+        int[][] table = matrix.multiple(3);
+        int[][] expect = {
+                {1, 2, 3},
+                {2, 4, 6},
+                {3, 6, 9}
+        };
+        assertThat(table, is(expect));
+    }
+
+    @Test
+    public void when4on4() {
+        Matrix matrix = new Matrix();
+        int[][] table = matrix.multiple(4);
+        int[][] expect = {
+                {1, 2, 3, 4},
+                {2, 4, 6, 8},
+                {3, 6, 9, 12},
+                {4, 8, 12, 16}
+        };
+        assertThat(table, is(expect));
+    }
+
+    @Test
+    public void when5on5() {
+        Matrix matrix = new Matrix();
+        int[][] table = matrix.multiple(5);
+        int[][] expect = {
+                {1, 2, 3, 4, 5},
+                {2, 4, 6, 8, 10},
+                {3, 6, 9, 12, 15},
+                {4, 8, 12, 16, 20},
+                {5, 10, 15, 20, 25}
+        };
+        assertThat(table, is(expect));
+    }
 }
