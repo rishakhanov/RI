3d43c483f0ff1d3e043cbd058a0c33124cd98918
rishakhanov <rishakhanov@rambler.ru>
2019-03-12 15:44:49 +0600

    1. Создать действия UserAction [#787]_update

--- chapter_002/src/main/java/ru/job4j/tracker/FindItem.java
+++ chapter_002/src/main/java/ru/job4j/tracker/FindItem.java
@@ -11,7 +11,9 @@ public class FindItem implements UserAction {
         System.out.println("----------------Поиск заявки по id------------");
         String id = input.ask("Введите id заявки : ");
         Item result = tracker.findById(id);
-        System.out.println("Заявка с id : " + result.getId() + " и с именем : " + result.getName());
+        if (result != null) {
+            System.out.println("Заявка с id : " + result.getId() + " и с именем : " + result.getName());
+        }
     }
 
     @Override
