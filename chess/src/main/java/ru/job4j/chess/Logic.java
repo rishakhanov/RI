8450067821a5de497b1fbe4797aa165a186f63aa
rishakhanov <rishakhanov@rambler.ru>
2019-04-13 23:19:10 +0600

    Каркас шахматной доски [#792]

---
+++ chess/src/main/java/ru/job4j/chess/Logic.java
@@ -0,0 +1,69 @@
+package ru.job4j.chess;
+
+import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.Figure;
+
+import java.util.Optional;
+
+/**
+ * //TODO add comments.
+ *
+ * @author Petr Arsentev (parsentev@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Logic {
+    private final Figure[] figures = new Figure[32];
+    private int index = 0;
+
+    public void add(Figure figure) {
+        this.figures[this.index++] = figure;
+    }
+
+    public boolean move(Cell source, Cell dest) throws FigureNotFoundException, OccupiedWayException {
+        boolean rst = false;
+        int index = this.findBy(source);
+        if (index == -1) {
+            throw new FigureNotFoundException("Figure Not Found.");
+        } else {
+            Cell[] steps = this.figures[index].way(source, dest);
+            for (int i = 0; i < steps.length; i++) {
+                if (this.findBy(steps[i]) != -1) {
+                    throw new OccupiedWayException("Occupied way.");
+                } else {
+                    rst = true;
+                    //this.figures[index] = this.figures[index].copy(dest);
+                }
+            }
+            this.figures[index] = this.figures[index].copy(dest);
+            //if (steps.length > 0 && steps[steps.length - 1].equals(dest)) {
+            //    rst = true;
+            //    this.figures[index] = this.figures[index].copy(dest);
+            //}
+        }
+        return rst;
+    }
+
+    public void clean() {
+        for (int position = 0; position != this.figures.length; position++) {
+            this.figures[position] = null;
+        }
+        this.index = 0;
+    }
+
+    private int findBy(Cell cell) {
+        int rst = -1;
+        for (int index = 0; index != this.figures.length; index++) {
+            if (this.figures[index] != null && this.figures[index].position().equals(cell)) {
+                rst = index;
+                break;
+            }
+        }
+        return rst;
+    }
+
+    //private boolean checkWay(int index) {
+    //    boolean check = false;
+    //
+    //}
+}
---
+++ chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
@@ -0,0 +1,44 @@
+package ru.job4j.chess.firuges.black;
+
+import ru.job4j.chess.ImpossibleMoveException;
+import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.Figure;
+
+/**
+ *
+ * @author Petr Arsentev (parsentev@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class BishopBlack implements Figure {
+    private final Cell position;
+
+    public BishopBlack(final Cell position) {
+        this.position = position;
+    }
+
+    @Override
+    public Cell position() {
+        return this.position;
+    }
+
+    @Override
+    public Cell[] way(Cell source, Cell dest) throws ImpossibleMoveException {
+        Cell[] steps = new Cell[Math.abs(source.y - dest.y)];
+        if (((source.y - dest.y) == 0) || ((source.x - dest.x) == 0)) {
+            throw new ImpossibleMoveException("Impossible Move");
+        } else {
+            for (int i = 0; i < steps.length; i++) {
+                steps[i] = Cell.values()[8 * source.x + source.y + (dest.x - source.x) / steps.length * (i + 1) * 8
+                        + (dest.y - source.y) / steps.length * (i + 1)];
+            }
+
+        }
+        return steps;
+    }
+
+    @Override
+    public Figure copy(Cell dest) {
+        return new BishopBlack(dest);
+    }
+}
