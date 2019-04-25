d5aa27b5490b55742835cd4dce845dc47fbb02c4
rishakhanov <rishakhanov@rambler.ru>
2019-04-25 09:37:12 +0600

    Каркас шахматной доски [#792]_update2

--- chess/src/main/java/ru/job4j/chess/Chess.java
+++ chess/src/main/java/ru/job4j/chess/Chess.java
@@ -60,13 +60,15 @@ public class Chess extends Application {
         );
         rect.setOnMouseReleased(
                 event -> {
-                    if (logic.move(this.findBy(momento.getX(), momento.getY()), this.findBy(event.getX(), event.getY())) == false) {
+                    try {
+                        if (logic.move(this.findBy(momento.getX(), momento.getY()), this.findBy(event.getX(), event.getY()))) {
+                            rect.setX(((int) event.getX() / 40) * 40 + 5);
+                            rect.setY(((int) event.getY() / 40) * 40 + 5);
+                        }
+                    } catch (ImpossibleMoveException exception) {
                         rect.setX(((int) momento.getX() / 40) * 40 + 5);
                         rect.setY(((int) momento.getY() / 40) * 40 + 5);
-                        throw new ImpossibleMoveException("Impossible Move");
                     }
-                    rect.setX(((int) event.getX() / 40) * 40 + 5);
-                    rect.setY(((int) event.getY() / 40) * 40 + 5);
                     /*
                     if (logic.move(this.findBy(momento.getX(), momento.getY()), this.findBy(event.getX(), event.getY()))) {
                         rect.setX(((int) event.getX() / 40) * 40 + 5);
