8916075411acb18e6283b0f32ab8024fede45f80
rishakhanov <rishakhanov@rambler.ru>
2019-06-17 14:08:00 +0600

    1. Организовать сортировку User [#10034]_update

--- chapter_003/src/main/java/sort/User.java
+++ chapter_003/src/main/java/sort/User.java
@@ -5,16 +5,14 @@ public class User implements Comparable<User> {
     public Integer age;
     String template = "str = '%s', num = %d";
 
-    User (String name, Integer age) {
+    User(String name, Integer age) {
         this.name = name;
-        this.age =age;
+        this.age = age;
     }
 
     @Override
     public int compareTo(User o) {
-        int result = age - o.age;
-
-        return result !=0 ? (int) result / Math.abs(result) : 0;
+        return age - o.age;
     }
 
     @Override
