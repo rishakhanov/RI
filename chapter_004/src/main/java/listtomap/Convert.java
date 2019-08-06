35774ae54464ca129a51209e50ea49168f1866f8
rishakhanov <rishakhanov@rambler.ru>
2019-08-06 15:48:07 +0600

    4. Преобразование List в Map. [#110226]_update2

--- chapter_004/src/main/java/listtomap/Convert.java
+++ chapter_004/src/main/java/listtomap/Convert.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 public class Convert {
 
     public Map<String, Student> returnmap(List<Student> studentList) {
-        return studentList.stream().distinct().collect(
+        return studentList.stream().collect(
                 Collectors.toMap(
                         e -> e.getName(),
                         e -> e
