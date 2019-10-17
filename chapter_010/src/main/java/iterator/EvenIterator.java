477bdf91688419e3bd59c254376960beaf3f9db6
rishakhanov <rishakhanov@rambler.ru>
2019-10-17 11:43:24 +0600

    5.1.2. Создать итератор четные числа[#188907]

---
+++ chapter_010/src/main/java/iterator/EvenIterator.java
@@ -0,0 +1,42 @@
+package iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class EvenIterator implements Iterator {
+
+    private final int[] values;
+    private int index = 0;
+
+    public EvenIterator(int[] values) {
+        this.values = values;
+    }
+
+    @Override
+    public boolean hasNext() {
+        boolean result = false;
+        for (int i = index; i < values.length; i++) {
+            if ((values.length > index) && (values[i] % 2 == 0)) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public Object next() throws NoSuchElementException {
+        int result = -1;
+        for (int i = index; i < values.length; i++) {
+            if (values[i] % 2 == 0) {
+                result = values[i];
+                index = i + 1;
+                break;
+            }
+        }
+        if (result == -1) {
+            throw new NoSuchElementException();
+        }
+        return result;
+    }
+}
