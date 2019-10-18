d26fa609dd33b7de4849131f8290954048155e6f
rishakhanov <rishakhanov@rambler.ru>
2019-10-18 11:14:37 +0600

    5.1.1. Итератор для двухмерного массива int[][][#188910]_update

--- chapter_010/src/main/java/iterator/IteratorArray.java
+++ chapter_010/src/main/java/iterator/IteratorArray.java
@@ -6,21 +6,15 @@ import java.util.Iterator;
 public class IteratorArray implements Iterator {
 
     private final int[][] values;
-    private int len, index, indexrow, indexcolumn  = 0;
+    private int index, indexrow, indexcolumn  = 0;
 
     public IteratorArray(int[][] values) {
         this.values = values;
-        for (int i = 0; i < values.length; i++) {
-            for (int j = 0; j < values[i].length; j++) {
-                len++;
-            }
-        }
-
     }
 
     @Override
     public boolean hasNext() {
-        return len > index;
+        return indexrow < values.length;
     }
 
     @Override
