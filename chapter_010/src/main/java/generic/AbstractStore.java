c93edb2d9249f0b7c1161882cfa90e1eab19ec4a
rishakhanov <rishakhanov@rambler.ru>
2019-11-02 10:49:01 +0600

    5.2.2. Реализовать Store<T extends Base>[#188913]_update

--- chapter_010/src/main/java/generic/AbstractStore.java
+++ chapter_010/src/main/java/generic/AbstractStore.java
@@ -1,6 +1,9 @@
 package generic;
 
-public abstract class AbstractStore<T> {
+public abstract class AbstractStore<T extends Base> implements Store<T> {
+
+    SimpleArray<T> objects = new SimpleArray<>(10);
+
     public int getIndex(T model, SimpleArray<T> objects) {
         int result = 0;
         while (objects.iterator().hasNext()) {
@@ -8,6 +11,48 @@ public abstract class AbstractStore<T> {
                 break;
             } else {
                 result++;
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public void add(T model) {
+        objects.add(model);
+    }
+
+    @Override
+    public boolean replace(String id, T model) {
+        T target = objects.get(getIndex(findById(id), objects));
+        objects.set(getIndex(findById(id), objects), model);
+        boolean result = true;
+        for (T item: objects) {
+            if (item.equals(target)) {
+                result = false;
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public boolean delete(String id) {
+        T target = objects.get(getIndex(findById(id), objects));
+        objects.remove(getIndex(findById(id), objects));
+        boolean result = true;
+        for (T item: objects) {
+            if (item.equals(target)) {
+                result = false;
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public T findById(String id) {
+        T result = null;
+        for (T item: objects) {
+            if (item.getId().equals(id)) {
+                result = item;
             }
         }
         return result;
