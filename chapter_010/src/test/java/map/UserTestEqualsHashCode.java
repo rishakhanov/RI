ce08d61d4f2e5210959af43ec9c99497cb3e6340
rishakhanov <rishakhanov@rambler.ru>
2019-11-30 15:55:20 +0600

    5. Перекрывать и equals и hashCode[#188897]

---
+++ chapter_010/src/test/java/map/UserTestEqualsHashCode.java
@@ -0,0 +1,48 @@
+package map;
+
+import org.junit.Test;
+
+import java.util.*;
+
+public class UserTestEqualsHashCode {
+    public static final class User {
+        String name;
+        int children;
+        Calendar birthday;
+
+        public User(String name, int children, Calendar birthday) {
+            this.name = name;
+            this.children = children;
+            this.birthday = birthday;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            User user = (User) o;
+            return children == user.children
+                    && name.equals(user.name)
+                    && birthday.equals(user.birthday);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name, children, birthday);
+        }
+    }
+
+    @Test
+    public void whenHashEqualsThen1Value() {
+        User first = new User("Petr", 2, new GregorianCalendar(1990, 2, 2));
+        User second = new User("Petr", 2, new GregorianCalendar(1990, 2, 2));
+        Map<User, String> map = new HashMap<>();
+        map.put(first, "first");
+        map.put(second, "second");
+        System.out.println(map);
+    }
+}
