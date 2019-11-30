a1f792dfe60727245cd52c31e5af3a50a362c6ea
rishakhanov <rishakhanov@rambler.ru>
2019-11-30 15:48:33 +0600

    4. Переопределить только equals[#188899]

---
+++ chapter_010/src/test/java/map/UserTestEquals.java
@@ -0,0 +1,43 @@
+package map;
+
+import org.junit.Test;
+
+import java.util.*;
+
+public class UserTestEquals {
+    public static final class User {
+        String name;
+        int children;
+        Calendar birthday;
+
+        public User(String name, int children, Calendar birthday) {
+            this.name = name;
+            this.children = children;
+            this.birthday = birthday;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            User user = (User) o;
+            return children == user.children
+                    && name.equals(user.name)
+                    && birthday.equals(user.birthday);
+        }
+    }
+
+    @Test
+    public void whenEqualsThen2Values() {
+        User first = new User("Petr", 2, new GregorianCalendar(1990, 2, 2));
+        User second = new User("Petr", 2, new GregorianCalendar(1990, 2, 2));
+        Map<User, String> map = new HashMap<>();
+        map.put(first, "first");
+        map.put(second, "second");
+        System.out.println(map);
+    }
+}
