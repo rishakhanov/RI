ed11a59caf221fd6ded7a781e99faa6880d82454
rishakhanov <rishakhanov@rambler.ru>
2019-12-20 13:47:20 +0600

    1. Создать модель User[#188896]_update

--- chapter_010/src/main/java/map/User.java
+++ chapter_010/src/main/java/map/User.java
@@ -4,9 +4,9 @@ import java.util.Calendar;
 
 public class User {
 
-    String name;
-    int children;
-    Calendar birthday;
+    private String name;
+    private int children;
+    private Calendar birthday;
 
     public User(String name, int children, Calendar birthday) {
         this.name = name;
@@ -14,4 +14,27 @@ public class User {
         this.birthday = birthday;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public void setChildren(int children) {
+        this.children = children;
+    }
+
+    public Calendar getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Calendar birthday) {
+        this.birthday = birthday;
+    }
 }
