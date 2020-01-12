726b662832fc391d48b651e9c0c24645addc7285
rishakhanov <rishakhanov@rambler.ru>
2020-01-12 18:28:48 +0600

    5.3.4. Задан связанный список. Определить цикличность.[#188893]_update

--- chapter_010/src/main/java/list/Cycle.java
+++ chapter_010/src/main/java/list/Cycle.java
@@ -2,6 +2,22 @@ package list;
 
 public class Cycle {
 
+    Node<Integer> first;
+    Node<Integer> two;
+    Node<Integer> third;
+    Node<Integer> four;
+
+    Cycle() {
+        first = new Node<Integer>(1);
+        two = new Node<Integer>(2);
+        third = new Node<Integer>(3);
+        four = new Node<Integer>(4);
+        first.next = two;
+        two.next = third;
+        third.next = four;
+        four.next = first;
+    }
+
     class Node<T> {
         T value;
         Node<T> next;
@@ -11,29 +27,26 @@ public class Cycle {
         }
     }
 
-    Node<Integer> first = new Node<Integer>(1);
-    Node<Integer> two = new Node<Integer>(2);
-    Node<Integer> third = new Node<Integer>(3);
-    Node<Integer> four = new Node<Integer>(4);
-    //Node<Integer> five = new Node<Integer>(5);
-
     public boolean hasCycle() {
-        boolean result = false;
-        first.next = two;
-        two.next = third;
-        third.next = four;
-        four.next = first;
-        Node[] objects = new Node[4];
-        objects[0] = first;
-        objects[1] = two;
-        objects[2] = third;
-        objects[3] = four;
+        Node tortoise = first;
+        Node hare = first;
 
-        for (int i = 1; i < objects.length; i++) {
-            if ((objects[i].next.equals(objects[i - 1])) || (objects[i].next.equals(objects[0]))) {
-                result = true;
+        while (true) {
+            tortoise = tortoise.next;
+
+            if (hare.next != null) {
+                hare = hare.next.next;
+            } else {
+                return false;
+            }
+
+            if ((tortoise == null) || (hare == null)) {
+                return false;
+            }
+
+            if (tortoise == hare) {
+                return true;
             }
         }
-        return result;
     }
 }
