445fce309b7fe170eb1582647d28abd1b75a2d9c
rishakhanov <rishakhanov@rambler.ru>
2020-01-12 13:19:20 +0600

    1. Создать элементарную структуру дерева[#188919]

---
+++ chapter_010/src/main/java/tree/Tree.java
@@ -0,0 +1,82 @@
+package tree;
+
+import java.util.*;
+
+public class Tree<E extends Comparable<E>> implements SimpleTree<E> {
+    private Node<E> root;
+    private int modCount = 0;
+
+    public Tree(E value) {
+        this.root = new Node<E>(value);
+    }
+
+    @Override
+    public boolean add(E parent, E child) {
+        boolean result = false;
+        Optional<Node<E>> parentNode = findBy(parent);
+        for (Node<E> currentChild : parentNode.get().leaves()) {
+            if (currentChild.eqValue(child)) {
+                return false;
+            }
+        }
+        parentNode.get().add(new Node<E>(child));
+        result = true;
+        modCount++;
+        return result;
+    }
+
+    @Override
+    public Optional<Node<E>> findBy(E value) {
+        Optional<Node<E>> rsl = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (el.eqValue(value)) {
+                rsl = Optional.of(el);
+                break;
+            }
+            for (Node<E> child : el.leaves()) {
+                data.offer(child);
+            }
+        }
+        return rsl;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return new Iterator() {
+            private final int expectedModCount = modCount;
+            Queue<Node<E>> elements = new LinkedList<>(Arrays.asList(root));
+
+            private void addToQueue(Node<E> item) {
+                for (Node<E> child : item.leaves()) {
+                    elements.offer(child);
+                }
+            }
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return elements.peek() != null;
+            }
+
+            @Override
+            public E next() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                addToQueue(elements.peek());
+                return elements.poll().getValue();
+            }
+        };
+    }
+
+
+
+}
---
+++ chapter_010/src/test/java/tree/TreeTest.java
@@ -0,0 +1,29 @@
+package tree;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+
+public class TreeTest {
+    @Test
+    public void when6ElFindLastThen6() {
+        Tree<Integer> tree = new Tree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        tree.add(4, 5);
+        tree.add(5, 6);
+        assertThat(tree.findBy(6).isPresent(), is(true));
+    }
+
+    @Test
+    public void when6ElFindNotExistThenOptionEpmty() {
+        Tree<Integer> tree = new Tree<>(1);
+        tree.add(1, 2);
+        assertThat(
+                tree.findBy(7).isPresent(), is(false)
+        );
+    }
+}
